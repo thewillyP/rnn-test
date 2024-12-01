61aadd14b627b3748b397775b03a3ddd06b9627a
thewillyp <williamsleepark@gmail.com>
2024-12-01 14:00:47 -0500

    data looks good

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+#%%
 from data import *
+import torch
 
-
+torch.manual_seed(0)
         
 @dataclass
 class Config:
@@ -24,12 +26,10 @@ def main(config: Config):
     test_loader = getDataLoaderIO(dataGenerator, config.t1, config.t2, ts, config.numTe, 1)
 
     for i, (xs, ys) in enumerate(train_loader):
-        print(f'Batch {i}:')
-        print(xs)
-        print(ys)
-        print('')
-        quit()
+        visualizeOutput(ys)
 
-config = Config(task=Random(), seq=10, numTr=10, numVl=10, numTe=10, batch_size_tr=1, batch_size_vl=2, t1=1, t2=2)
+config = Config(task=Random(), seq=10, numTr=100, numVl=10, numTe=10, batch_size_tr=100, batch_size_vl=2, t1=1, t2=2)
 
 main(config)
+
+# %%
