ad0674df903c4a91368c3c0e10f255386a0dcd87
thewillyp <williamsleepark@gmail.com>
2024-12-09 13:52:52 -0500

    new exp

--- configs.py
+++ configs.py
@@ -3,43 +3,42 @@ import itertools
 # Parameter grid
 params = {
     "n_in": [2],
-    "n_h": [200, 30],
+    "n_h": [200],
     "n_out": [1],
     "num_layers": [1],
     "task": ["Random"],
-    "seq": [20, 30],
+    "seq": [20],
     "numVl": [5000],
     "numTe": [5000],
     "batch_size_vl": [5000],
     "batch_size_te": [5000],
     "num_epochs": [10000], 
-    "learning_rate": [0.01, 0.1, 0.001],
+    "learning_rate": [0.01],
     "optimizerFn": ["SGD"],
     "lossFn": ["mse"],
     "mode": ["experiment"],
-    "checkpoint_freq": [5000],
-    "seed": list(range(1, 2+1)),
-    "projectName": ["rnn-test-ohotest"],
+    "checkpoint_freq": [100],
+    "seed": list(range(1, 5+1)),
+    "projectName": ["rnn-test-ohotest2"],
     "logger": ["wandb"],
     "performance_samples": [9],
     "init_scheme": ['RandomInit'],
-    "activation_fn": ['relu', 'tanh'],
+    "activation_fn": ['tanh', 'relu'],
     "log_freq": [1],
     "meta_learning_rate": [1e-4, 1e-5],
     "l2_regularization": [0],
-    "is_oho": [1, 0],
+    "is_oho": [1],
 }
 
 # Pairing for numTr and batch_size_tr
 train_batch_combinations = [
-    {"numTr": 5000, "batch_size_tr": 5000},
     {"numTr": 5000, "batch_size_tr": 100},
+    {"numTr": 5000, "batch_size_tr": 5000},
 ]
 
 # Convert t_combinations to dictionary style
 t_combinations = [
     {"t1": 3, "t2": 5},
-    {"t1": 11, "t2": 14},
 ]
 
 # Generate all combinations
