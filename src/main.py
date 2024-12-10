scuffed fix later added more logging to check for numerical isntability

src/main.py
@@ -167,6 +167,8 @@ def get_grad_valid(config: Config, model, data, target):
     #     s = val_model.activations[:, -1, :].clone().detach().unsqueeze(0)
 
     grad_val = get_grads(val_model)
+
+    wandb.log({"validation_loss": loss.item()}, commit=False)
     
     return grad_val
 
@@ -232,7 +234,9 @@ def train(config: Config, logger: Logger, model: RNN, train_loader: Iterator, va
                         , "validation_gradient_norm": torch.linalg.norm(grad_valid, 2).item()
                         , "dFdlr_tensor_norm": torch.linalg.norm(model.dFdlr, 2).item()
                         , "dFdl2_tensor_norm": torch.linalg.norm(model.dFdl2, 2).item()
-                        , "parameter_norm": torch.linalg.norm(torch.nn.utils.parameters_to_vector(model.parameters()), 2).item()}
+                        , "parameter_norm": torch.linalg.norm(torch.nn.utils.parameters_to_vector(model.parameters()), 2).item()
+                        , "iteration": epoch * len(train_loader) + i
+                        , "epoch": epoch}
                 
                 # l2 = lambda x: torch.linalg.norm(x, 2)
                 # vmapped = torch.vmap(torch.vmap(l2))(model.activations)
@@ -242,11 +246,13 @@ def train(config: Config, logger: Logger, model: RNN, train_loader: Iterator, va
                 logger.log(log_data)
         
         logger.log({"test_loss": test_loss(config, test_loader, model),
-                    "wave_test_loss": wave_test_loss(config, model, test_loader_)})
+                    "wave_test_loss": wave_test_loss(config, model, test_loader_),
+                    "epoch": epoch})
         if (epoch+1) % config.checkpointFrequency == 0:
             log_modelIO(config, logger, model, f"epoch_{epoch}")
             logger.log({"performance": visualize(config, model, test_ds),
-                        "wave_performance": visualize(config, model, test_ds_)})
+                        "wave_performance": visualize(config, model, test_ds_),
+                        "epoch": epoch})
 
     return model
 

src/rnn.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 from records import InitType, RnnConfig, ZeroInit, RandomInit, StaticRandomInit
-
+import wandb
 
 def block_orthogonal_init(size, block_size, frequency=torch.pi/8, amplitude=(0.9, 0.999)):
     W = torch.zeros((size, size))
@@ -125,8 +125,19 @@ class RNN(nn.Module):
     def update_dFdlr(self, Hv, param, grad):
         self.Hlr = self.eta*Hv
         self.Hlr_norm = torch.norm(self.Hlr, p=2)
-        self.dFdlr_norm = torch.norm(self.dFdlr)
+        self.dFdlr_norm = torch.norm(self.dFdlr, p=2)
         self.dFdlr.data = self.dFdlr.data * (1-2*self.lambda_l2*self.eta) - self.Hlr - grad - 2*self.lambda_l2*param
+        wandb.log({"dFdlr_hv_norm": torch.norm(Hv)
+                , "dFdlr_lr": self.eta
+                , "Hlr_norm": self.Hlr_norm
+                , "dFdlr_norm": self.dFdlr_norm
+                , "dFdlr_norm_vl": self.grad_norm_vl
+                , "dFdlr_coeff": (1-2*self.lambda_l2*self.eta)
+                , "dFdlr_coeff_norm": torch.norm(self.dFdlr.data * (1-2*self.lambda_l2*self.eta), p=2)
+                , "dFdlr_param_norm": torch.norm(param, p=2)
+                , "dFdlr_grad_norm": torch.norm(grad, p=2)
+                , "dFdlr_coeff_param_norm": torch.norm(2*self.lambda_l2*param, p=2)
+                }, commit=False)
 
     def update_dFdlambda_l2(self, Hv, param):
         self.Hl2 = self.eta*Hv
@@ -137,6 +148,7 @@ class RNN(nn.Module):
     def update_eta(self, mlr, val_grad):
         val_grad = torch.nn.utils.parameters_to_vector(val_grad)
         delta = val_grad.dot(self.dFdlr).data.item()
+        wandb.log({"dFdlr_dot_val_grad": delta}, commit=False)
         # delta = torch.dot(val_grad, self.dFdlr).item()
         self.eta -= mlr * delta
         self.eta = max(0.0, self.eta)