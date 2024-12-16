c68f2c5023e28dff252edb21fa97e217c804a0e1
thewillyp <williamsleepark@gmail.com>
2024-12-16 04:26:33 -0500

    exp 6

--- configs.py
+++ configs.py
@@ -1,48 +1,47 @@
 import itertools
+import torch 
+
+torch.manual_seed(0)
+
+# sample uniform between [0.005, 0.06] learning rate
+learning_rates = torch.rand(20) * 0.055 + 0.005
 
 # Parameter grid
 params = {
     "n_in": [2],
-    "n_h": [200, 30],
+    "n_h": [30],
     "n_out": [1],
     "num_layers": [1],
     "task": ["Random"],
     "seq": [100],
-    "numVl": [500],
+    "numVl": [2],
     "numTe": [5000],
-    "batch_size_vl": [500],
+    "numTr": [4], 
+    "batch_size_tr": [2],
+    "batch_size_vl": [4],
     "batch_size_te": [5000],
-    "num_epochs": [10000], 
-    "learning_rate": [0.1, 0.01, 0.3],
+    "num_epochs": [1600], 
+    "learning_rates": learning_rates,
+    # "learning_rate": [0.1, 0.01, 0.3],
     "optimizerFn": ["SGD"],
     "lossFn": ["mse"],
     "mode": ["experiment"],
-    "checkpoint_freq": [2000],
-    "seed": list(range(1, 2+1)),
-    "projectName": ["rnn-test-ohotest3"],
+    "checkpoint_freq": [800],
+    "seed": list(range(3, 3+1)),
+    "projectName": ["oho-rnn-generalization-gap"],
     "logger": ["wandb"],
     "performance_samples": [9],
-    "init_scheme": ['ZeroInit'],
-    "activation_fn": ['relu', 'tanh'],
-    "log_freq": [2],
+    "init_scheme": ['StaticRandomInit'],
+    "activation_fn": ['tanh'],
+    "log_freq": [1],
     "meta_learning_rate": [1e-4],
     "l2_regularization": [0],
     "is_oho": [1],
     "randomType": ["Uniform"],
-    "time_chunk_size": [10]
+    "time_chunk_size": [10],
+    "t1": [3],
+    "t2": [5],
 }
-
-# Pairing for numTr and batch_size_tr
-train_batch_combinations = [
-    {"numTr": 500, "batch_size_tr": 500},
-    {"numTr": 500, "batch_size_tr": 100},
-]
-
-# Convert t_combinations to dictionary style
-t_combinations = [
-    {"t1": 3, "t2": 5},
-    {"t1": 9, "t2": 1},
-]
 
 # Generate all combinations
 keys, values = zip(*params.items())
@@ -52,12 +51,13 @@ other_combinations = itertools.product(*values)
 final_combinations = []
 for combo in other_combinations:
     base_combo = dict(zip(keys, combo))
-    for train_batch in train_batch_combinations:
-        for t_combo in t_combinations:
-            new_combo = base_combo.copy()
-            new_combo.update(train_batch)
-            new_combo.update(t_combo)
-            final_combinations.append(new_combo)
+    final_combinations.append(base_combo)
+    # for train_batch in train_batch_combinations:
+    #     for t_combo in t_combinations:
+    #         new_combo = base_combo.copy()
+    #         new_combo.update(train_batch)
+    #         new_combo.update(t_combo)
+    #         final_combinations.append(new_combo)
 
 # Write to configs.txt
 with open("configs.txt", "w") as file:
